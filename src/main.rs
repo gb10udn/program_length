04403bcd795d85c622afa5b9a59daa2cd8b1244d
gb10udn <rbbpx0926@gmail.com>
2024-01-13 12:33:43 +0900

    行数をカウントする関数を追加した。

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ use std::fs::File;
 use walkdir::WalkDir;
 
 
+// tokei という Rust 製のソースコード長を取得するコマンドラインツールが使いやすかったのでそれを使う。(なので、これは練習用。)
+// https://github.com/XAMPPRocky/tokei
+// .vue ファイルのカウントが弱い issue が 2021 年に上がっているが、2024-01 時点で改善されていないので、その部分は作る価値があるかも？
+// https://github.com/XAMPPRocky/tokei/issues/784
+// あとは、関数とかクラスの数を数えても面白いかも？
+
 fn main() {
     let user_input = obtain_user_input();
     let user_input = remove_head_and_tail_double_quotation(user_input);
     let extension = "rs";  // TODO: 240112 拡張子は外部から指定できるようにせよ。
 
-    let path_list = match retrieve_files(&user_input as &str, extension) {
+    let flist = match retrieve_files(&user_input as &str, extension) {
         Some(val) => val,
         None => {
             println!("Error: No file exists (user_input: {}, extension: {})", user_input, extension);
@@ -17,11 +23,11 @@ fn main() {
         },
     };
 
-    for path in path_list {
-        if let Ok(text) = open_text_file(&path) {
-            println!("{}", text);
-        }
-    }
+    // for path in flist {
+    //     if let Ok(text) = open_text_file(&path) {
+    //         println!("{}", text);
+    //     }
+    // }
 
     // stop();  // TODO: 240113 本番では有効にする。
 }
@@ -51,6 +57,7 @@ fn remove_head_and_tail_double_quotation(arg: String) -> String {
 
 
 /// base_dir 配下の、拡張子が extension のファイルのリストを取得する。
+/// HACK: 240113 ネストが深くて読みにくいので、修正せよ。
 fn retrieve_files(base_dir: &str, target_extension: &str) -> Option<Vec<String>> {  // TODO: 240112 複数の拡張子にも対応すること (１つのプロジェクトから、拡張子を複数とかできないのかな？)
     let mut result: Vec<String> = Vec::new();
     for entry in WalkDir::new(base_dir) {  // FIXME: 240112 .max_depth() を設定しない場合、どれくらいの階層まで探すかよくわからない。
@@ -72,16 +79,23 @@ fn retrieve_files(base_dir: &str, target_extension: &str) -> Option<Vec<String>>
 }
 
 
+#[allow(dead_code)]  // INFO: 240113 将来用。改行コード等をカウントする用途。
 fn open_text_file(path: &str) -> Result<String, io::Error> {
     let mut f = File::open(path)?;
     let mut result = String::new();
-    
     f.read_to_string(&mut result)?;
     Ok(result)
 }
 
 
-fn stop() {
+fn count_row_num(path: &str) -> Result<usize, io::Error> {
+    let f = File::open(path)?;
+    let buf = std::io::BufReader::new(f);  // INFO: 240113 BufReader を使うと処理高速らしい。
+    Ok(buf.lines().count())
+}
+
+
+fn _stop() {
     println!("");
     println!("finished !!! Please input enter key");
     let mut a = String::new();
@@ -91,6 +105,7 @@ fn stop() {
 
 #[cfg(test)]
 mod tests {
+    const TEST_PATH: &str = "./misc/test1.txt";
     #[test]
     fn test_retrieve_files() {
         use crate::retrieve_files;
@@ -101,7 +116,12 @@ mod tests {
     #[test]
     fn test_open_text_file() {
         use crate::open_text_file;
-        let result = open_text_file(".\\misc\\test1.txt").unwrap();  // HACK: 240112 ./misc にテキストファイルを入れてテストが微妙な気もするので、何か考えるべきかも？
-        assert_eq!(result, String::from("あいうえお"));
+        assert_eq!(open_text_file(TEST_PATH).unwrap(), String::from("あいうえお"));  // HACK: 240112 ./misc にテキストファイルを入れてテストが微妙な気もするので、何か考えるべきかも？
+    }
+
+    #[test]
+    fn test_count_row_num() {
+        use crate::count_row_num;
+        assert_eq!(count_row_num(TEST_PATH).unwrap(), 1);
     }
 }
